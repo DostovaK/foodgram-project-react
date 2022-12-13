985cde0cc65fd0157589c7e88bc9ab5ebd0898cc
Kseniya Dostova <kdostova@yandex.ru>
2022-12-13 14:27:40 +0400

    without #

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'corsheaders.middleware.CorsMiddleware',
+#    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'foodgram.urls'
