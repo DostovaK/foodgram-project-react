ad062d8cca0cbaa4294ded53d05016d8795eb17b
Kseniya Dostova <kdostova@yandex.ru>
2022-12-14 22:40:16 +0400

    requirements changed

--- backend/api/utils.py
+++
@@ -1,9 +0,0 @@
-def if_is_in_fav_or_shop_list(self, obj, model):
-    if (
-        self.context.get('request') is not None
-        and self.context.get('request').user.is_authenticated
-    ):
-        return model.objects.filter(
-            user=self.context.get('request').user, recipe=obj
-        ).exists()
-    return False
