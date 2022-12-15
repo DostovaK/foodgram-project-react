a5222f92c056c01ebe80e0d72b7c2ed8b5915075
Kseniya Dostova <kdostova@yandex.ru>
2022-12-15 17:52:36 +0400

    requirements changed

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers, status
@@ -7,7 +8,6 @@ from rest_framework.fields import SerializerMethodField
 from recipes.models import (Favorite, Ingredient, IngredientRecipe, Recipe,
                             ShoppingCart, Tag)
 from users.models import Follow, User
-from django.shortcuts import get_object_or_404
 
 
 class UserSerializer(UserSerializer):
